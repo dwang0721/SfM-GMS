00fed16420199a0bcee5928e2bb7a003c99c5578
DiWang <dwang0721@gmail.com>
2021-05-02 10:39:17 -0700

    baseline code

---
+++ SfM-GMS/SfM-GMS/sfm.cpp
@@ -0,0 +1,30 @@
+// Program0.cpp
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
+
+// main - a quick test of OpenCV			
+int main(int argc, char* argv[])
+{
+    Mat image = imread("../SourceImages/L1.jpg");
+
+    namedWindow("Original Image");
+    imshow("Original Image", image);
+    waitKey(0);
+
+    for (int r = 0; r < image.rows; r++) {
+        for (int c = 0; c < image.cols; c++) {
+            for (int b = 0; b < 3; b++) {
+                image.at<Vec3b>(r, c)[b] = 255 - image.at<Vec3b>(r, c)[b];
+            }
+        }
+    }
+
+    namedWindow("Photonegative");
+    imshow("Photonegative", image);
+    waitKey(0);
+
+    imwrite("negative.jpg", image);
+    return 0;
+}
