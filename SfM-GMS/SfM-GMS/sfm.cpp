6b9cfba136a3cca0e5af74963512f6afbecf8637
DiWang <dwang0721@gmail.com>
2021-05-08 12:05:12 -0700

    baseline disparity

--- SfM-GMS/SfM-GMS/sfm.cpp
+++ SfM-GMS/SfM-GMS/sfm.cpp
@@ -1,29 +1,85 @@
-// Program0.cpp
+#include "sfm.h"
+int alg = STEREO_SGBM;
 
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-using namespace cv;
+// learning reference:
+// https://jayrambhia.com/blog/disparity-mpas
+
+// Tasks:
+// 1. parser and image input 
+// 2. LOGOS application for SfM
+// 3. GMS application for SfM
+// 4. baseline implementation
 
 // main - a quick test of OpenCV			
 int main(int argc, char* argv[])
 {
-    Mat image = imread("../SourceImages/L1.jpg");
+    CommandLineParser parser(argc, argv, parserKeys);
+    processParser(parser);
 
-    namedWindow("Original Image");
-    imshow("Original Image", image);
+    // initialize input
+    const Mat img1 = imread("../SourceImages/Disparity_L.jpg");
+    const Mat img2 = imread("../SourceImages/Disparity_R.jpg");
+    Mat disp;
+
+    namedWindow("Left Eye Image", WINDOW_NORMAL);
+    resizeWindow("Left Eye Image", img1.cols/4, img1.rows/4);
+    imshow("Left Eye Image", img1);
+    
+    namedWindow("Right Eye Image", WINDOW_NORMAL);
+    resizeWindow("Right Eye Image", img2.cols/4, img2.rows/4);
+    imshow("Right Eye Image", img2);    
     waitKey(0);
 
-    for (int r = 0; r < image.rows; r++) {
-        for (int c = 0; c < image.cols; c++) {
-            for (int b = 0; b < 3; b++) {
-                image.at<Vec3b>(r, c)[b] = 255 - image.at<Vec3b>(r, c)[b];
-            }
-        }
-    }
-
-    namedWindow("Photonegative");
-    imshow("Photonegative", image);
+    // Stereo: disparity map
+    cout << "Start stereo matching..." << endl;
+    stereo_match(img1, img2, disp);
+    namedWindow("Disparity Map", WINDOW_NORMAL);
+    imshow("Disparity Map", disp);
+    cout << "Done matching" << endl;
     waitKey(0);
 
     return 0;
+}
+
+// ----------------------------------
+// ---- Function Implementation -----
+// ----------------------------------
+void processParser(CommandLineParser parser){
+    if (parser.has("help"))
+    {
+        printHelp();
+    }
+
+    if (parser.has("algorithm"))
+    {
+        std::string _alg = parser.get<string>("algorithm");
+        if (_alg == "SGBM"){
+            alg = STEREO_SGBM;
+        }
+        // continue other algorithm
+    }
+}
+
+void printHelp(){
+   printf("\nhelp message here!");
+}
+
+void stereo_match(const Mat &img1, const Mat &img2, Mat &disparity){
+    // place holder algorithm
+    Mat g1, g2;
+    cvtColor(img1, g1, COLOR_BGR2GRAY);
+    cvtColor(img2, g2, COLOR_BGR2GRAY);
+
+    Ptr<StereoBM> sbm = StereoBM::create(16, 9);
+    sbm->setNumDisparities(112);
+    sbm->setPreFilterSize(5);
+    sbm->setPreFilterCap(61);
+    sbm->setMinDisparity(-39);
+    sbm->setTextureThreshold(507);
+    sbm->setUniquenessRatio(0);
+    sbm->setSpeckleWindowSize(0);
+    sbm->setSpeckleRange(8);
+    sbm->setDisp12MaxDiff(1);
+    sbm->compute(g1, g2, disparity);
+    normalize(disparity, disparity, 0, 255, NORM_MINMAX, CV_8U);
 }
---
+++ SfM-GMS/SfM-GMS/sfm.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/xfeatures2d.hpp>
+
+#include <pcl/common/common_headers.h>
+
+using namespace cv;
+using namespace std;
+
+// ----------------------------------
+// ----------- Constant -------------
+// ----------------------------------
+const String parserKeys =
+"{help h usage ? |      | print this message   }"
+"{@image1        |      | image1 for compare   }"
+"{@image2        |<none>| image2 for compare   }"
+"{algorithm      |      | LOGOS or GMS         }"
+;
+
+enum { STEREO_SGBM = 1, LOGOS = 2, GMS = 3};
+
+// ----------------------------------
+// ---- Function Declaration --------
+// ----------------------------------
+void processParser(CommandLineParser parser);
+void printHelp();
+void stereo_match(const Mat &img1, const Mat &img2, Mat &disparity);
