2aaaa28c27e1dd286858a842ca6383835aba425b
DiWang <dwang0721@gmail.com>
2021-05-15 15:48:34 -0700

    calibration ready

--- SfM-GMS/SfM-GMS/sfm.cpp
+++ SfM-GMS/SfM-GMS/sfm.cpp
@@ -9,51 +9,41 @@ int alg = STEREO_SGBM;
 //     a. how to use match points for disparity map <--- Ahmed
 //     b. how to use match points for reconstruction 3d. <--- Di
 
-
 // main - a quick test of OpenCV			
 int main(int argc, char* argv[])
 {
     // ToDo: parser implementation need
-    CommandLineParser parser(argc, argv, parserKeys);
-    processParser(parser);
+    //CommandLineParser parser(argc, argv, parserKeys);
+    //processParser(parser);
 
-    // initialize input
-    const Mat img1 = imread("../SourceImages/Disparity_L.jpg");
-    const Mat img2 = imread("../SourceImages/Disparity_R.jpg");
-    Mat disp;
 
-    Mat img3 = imread("../SourceImages/view0-1.png");
-    Mat img4 = imread("../SourceImages/view2-1.png");
-    //cout << "Start background blurring..." << endl;
-    //blurDistant(img3, img4, disp);
-    //cout << "Done" << endl;
+    // ------------- Disparity Map --------------------  
+    const Mat img5 = imread("../SourceImages/Disparity_L.jpg");
+    const Mat img6 = imread("../SourceImages/Disparity_R.jpg");
+    Ptr<SIFT> detector = SIFT::create();
+    vector<KeyPoint> keypoints1, keypoints2;
+    Mat descriptor1, descriptor2;
+    Mat image_show;
+    // Obtain keypoints and descriptors
+    detector->detectAndCompute(img5, noArray(), keypoints1, descriptor1);
+    detector->detectAndCompute(img6, noArray(), keypoints2, descriptor2);
+    Ptr<BFMatcher> matcher = BFMatcher::create();
+    vector<DMatch> matches, matchesGMS;
+    // Match two images' descriptors
+    // matcher->match(descriptor1, descriptor2, matches);
+    cv::xfeatures2d::matchGMS(img5.size(), img6.size(), keypoints1, keypoints2, matches, matchesGMS);    
+    drawMatches(img5, keypoints1, img6, keypoints2, matchesGMS, image_show);
+    namedWindow("Match Image", WINDOW_NORMAL);
+    float SCALE = 1.0;
+    // Scale down the window size
+    resizeWindow("Match Image", image_show.cols / SCALE, image_show.rows / SCALE);
+    imshow("Match Image", image_show);
     waitKey(0);
-
-    namedWindow("Left Eye Image", WINDOW_NORMAL);
-    resizeWindow("Left Eye Image", img1.cols/4, img1.rows/4);
-    imshow("Left Eye Image", img1);
-    
-    namedWindow("Right Eye Image", WINDOW_NORMAL);
-    resizeWindow("Right Eye Image", img2.cols/4, img2.rows/4);
-    imshow("Right Eye Image", img2);    
-    waitKey(0);
-
-    // Stereo: disparity map
-    cout << "Start stereo matching..." << endl;
-    stereo_match(img1, img2, disp);
-    namedWindow("Disparity Map", WINDOW_NORMAL);
-    imshow("Disparity Map", disp);
-    cout << "Done matching" << endl;
-    waitKey(0);
-
-    // ToDo: Disparity Map
 
     // --------- SfM Implementation --------------
-    Mat imgL = imread("../SourceImages/L1.jpg");
-    Mat imgR = imread("../SourceImages/M1.jpg");
-    structureFromMotion(imgL, imgR);
-   
-    return 0;
+    //Mat imgL = imread("../SourceImages/L1.jpg");
+    //Mat imgR = imread("../SourceImages/M1.jpg");
+    //structureFromMotion(imgL, imgR);
 }
 
 // ----------------------------------
@@ -77,26 +67,6 @@ void processParser(CommandLineParser parser){
 
 void printHelp(){
    printf("\nhelp message here!");
-}
-
-void stereo_match(const Mat &img1, const Mat &img2, Mat &disparity){
-    // place holder algorithm
-    Mat g1, g2;
-    cvtColor(img1, g1, COLOR_BGR2GRAY);
-    cvtColor(img2, g2, COLOR_BGR2GRAY);
-
-    Ptr<StereoBM> sbm = StereoBM::create(16, 9);
-    sbm->setNumDisparities(112);
-    sbm->setPreFilterSize(5);
-    sbm->setPreFilterCap(61);
-    sbm->setMinDisparity(-39);
-    sbm->setTextureThreshold(507);
-    sbm->setUniquenessRatio(0);
-    sbm->setSpeckleWindowSize(0);
-    sbm->setSpeckleRange(8);
-    sbm->setDisp12MaxDiff(1);
-    sbm->compute(g1, g2, disparity);
-    normalize(disparity, disparity, 0, 255, NORM_MINMAX, CV_8U);
 }
 
 void blurDistant( Mat& img1, Mat& img2, Mat& disparity) {
