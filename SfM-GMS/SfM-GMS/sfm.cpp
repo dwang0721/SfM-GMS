51e4f79cf94f136055fb7e5768d7200f0575c744
DiWang <dwang0721@gmail.com>
2021-05-15 15:55:33 -0700

    some sad mistakes

--- SfM-GMS/SfM-GMS/sfm.cpp
+++ SfM-GMS/SfM-GMS/sfm.cpp
@@ -44,6 +44,8 @@ int main(int argc, char* argv[])
     //Mat imgL = imread("../SourceImages/L1.jpg");
     //Mat imgR = imread("../SourceImages/M1.jpg");
     //structureFromMotion(imgL, imgR);
+
+    return 0;
 }
 
 // ----------------------------------
@@ -67,29 +69,5 @@ void processParser(CommandLineParser parser){
 
 void printHelp(){
    printf("\nhelp message here!");
-}
-
-void blurDistant( Mat& img1, Mat& img2, Mat& disparity) {
-    cout << "inside blurDistant" << endl;
-    Mat image_blurred;
-    stereo_match(img1,img2,disparity);
-    cout << "Image size is " << img1.rows << " and " << img1.cols << endl;
-    imshow("Blur Distant - Disparity image", disparity);
-    GaussianBlur(img1, image_blurred, Size(15,15), 0);
-    cv::Size s = disparity.size();
-    cout << "disparity matrix is " << s.height << " and " << s.width << endl;
-    for (int i = 0; i < img1.rows-3; i++) {
-        cout << i << endl;
-        for (int j = 0; j < img1.cols-3; j++) {
-            if (disparity.at<int>(i, j) >= 0 && disparity.at <int> (i, j) <= 10) {
-                image_blurred.at<cv::Vec3b>(i, j)[0] = img1.at<cv::Vec3b>(i, j)[0];
-                image_blurred.at<cv::Vec3b>(i, j)[1] = img1.at<cv::Vec3b>(i, j)[1] ;
-                image_blurred.at<cv::Vec3b>(i, j)[2] = img1.at<cv::Vec3b>(i, j)[2] ;
-            }
-        }
-    }
-    namedWindow("Blurred imaged based on depth", WINDOW_NORMAL);
-    imshow("Blurred imaged based on depth", image_blurred);
-    waitKey();
 }
 
